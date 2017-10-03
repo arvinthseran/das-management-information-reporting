9eb0ee089f9d107fbf9d2930319cb8ba2871eb8e
Arvinth Seran <arvinth.seran@gmail.com>
2017-10-03 16:50:19 +0100

    added GetAllApprenticeshipStandards endpoint

--- Esfa.Das.Reporting/Esfa.Das.Reporting.Client/ReportingClient.cs
+++ Esfa.Das.Reporting/Esfa.Das.Reporting.Client/ReportingClient.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SFA.DAS.Apprenticeships.Api.Types.Providers;
 using Esfa.Das.Reporting.Types;
+using Esfa.Das.Reporting.Types.Apprenticeship;
+using System;
 
 namespace Esfa.Das.Reporting.Client
 {
-    public class ReportingClient : ReportingClientBase, IRepotingClient
+    public class ReportingClient : ReportingClientBase, IReportingClient
     {
-
+        private ProviderReportingClient _providerReportingClient;
+        private ApprenticeshipStandardReportingClient _apprenticeshipStandardReportingClient;
 
         public ReportingClient(string baseUri = null, string cdUri = null) : base(baseUri, cdUri)
         {
+            _providerReportingClient = new ProviderReportingClient(apprenticeshipServiceUri, courceDirectoryUri);
+            _apprenticeshipStandardReportingClient = new ApprenticeshipStandardReportingClient(apprenticeshipServiceUri, courceDirectoryUri);
         }
 
         public IEnumerable<ProviderSummary> GetAllMainProviders()
@@ -23,12 +25,16 @@ namespace Esfa.Das.Reporting.Client
 
         public IEnumerable<ProviderLocations> GetAllMainProviderLocations()
         {
-            return FindAllProvidersLocations(GetAllMainProviders().ToList().Select(x => int.Parse(x.Ukprn.ToString())).ToList());
+            return _providerReportingClient.FindAllProvidersLocations(GetAllMainProviders().ToList().Select(x => int.Parse(x.Ukprn.ToString())).ToList());
         }
 
-        public new void DownloadProviderDetails(List<int> providerUkprns)
+        public void DownloadProviderDetails(List<int> providerUkprns)
         {
-            base.DownloadProviderDetails(providerUkprns);
+            _providerReportingClient.DownloadProviderDetails(providerUkprns);
+        }
+        public IEnumerable<ApprenticeshipStandard> GetAllApprenticeshipStandards()
+        {
+            return _apprenticeshipStandardReportingClient.GetAllApprenticeshipStandards();
         }
     }
 }
